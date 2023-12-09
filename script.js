c56a2cd8703a3f06420096667e6267c3f932cf67
merry29 <merrimalaquias@gmail.com>
2023-12-09 10:25:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let nome = 'merry'
+
+let middleName = 'esperança'
+
+let fullName = nome + ' ' + middleName;
+
+
+console.log(fullName)
+
+let idade = 25;
+let ano = 2021
+
+let sum = idade + ano
+
+console.log(sum);
+
+let pizza = 2
+let birth = 2000
+
+let total = pizza * birth;
+
+console.log(total);
+
+
+let like = 2;
+let slices = 40;
+let amigos = 4;
+
+let operação = (like * slices)/ amigos;
+
+console.log(operação)
+
+
+function verificarIdade(anos){
+    if (diferencaEmAnos >= 18) {
+        return "Maior de 18 anos";
+      } else {
+        return "Menor de 18 anos";
+      }
+    }
+
+anos(14)
+
